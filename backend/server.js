1f948f94b0a9ae949d57bc6a2414ee104af359af
Arpayitri Bhattacharyya <arpayitribhattacharyya090305@gmail.com>
2026-01-22 23:10:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const app = express();
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 app.use("/api/researchers", researcherRoutes);
